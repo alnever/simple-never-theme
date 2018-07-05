a1cfbd52744c0f2cf41e5276ae5fb0d1287d4434
Alex Neverov <al_neverov@live.ru>
2018-07-05 00:18:50 -0700

    Comments added

---
+++ comments.php
@@ -0,0 +1,52 @@
+<?php 
+    if (post_password_required())
+        return;
+?>
+
+<div id="comments" class="comments-area">
+<?php if ( have_comments() ): ?>
+    <h4 class="comments-title">
+        <?php 
+            printf(__('Comments: %1$s','simple-never-theme' ),
+                    number_format_i18n( get_comments_number() )
+                  );
+        ?>
+    </h4>
+    
+    <!-- start comment list -->
+    <ul class="comment-list">
+      <?php
+        wp_list_comments (
+            array(
+                'style' => 'ul',
+                'short_ping' => true,
+                'avatar_size' => 50,
+                'max_depth' => 10
+            )
+        );
+      ?>
+    </ul>
+    <!-- end comment list -->
+    
+    <!-- comments pagination -->
+    <?php 
+        if (get_comment_pages_count() > 1 && get_option('page_comments')):
+    ?>
+        <nav class="navigation comment-navigation" role="navigation">
+            <!-- h4 class="screen-reader-text section-heading"><?php _e( 'Comment navigation', 'simple-never-theme' ); ?></h4-->
+            <div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'simple-never-theme' ) ); ?></div>
+            <div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'simple-never-theme' ) ); ?></div>
+        </nav>
+    <?php endif; ?>
+    <!-- end comments pagination -->
+    
+    <!-- no comments -->
+    <?php if ( ! comments_open() && get_comments_number() ) : ?>
+        <p class="no-comments"><?php _e( 'Comments are closed.' , 'simple-never-theme' ); ?></p>
+    <?php endif; ?>    
+    
+<?php endif; ?>
+    
+<!-- comments form -->
+<?php comment_form(); ?>
+</div>
--- single.php
+++ single.php
@@ -33,6 +33,12 @@
                         ?>
                     </p>
                 </div>
+                <!-- comments -->    
+                <?php 
+                    if ( comments_open() || get_comments_number() ) :
+                        comments_template();
+                    endif;
+                ?>
             </div>
         <?php endwhile; ?>
         <div class="navigation">
