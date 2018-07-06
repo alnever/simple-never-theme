670cd04d4d6d7449e66de9c31e5ffe3d1ec62292
Alex Neverov <al_neverov@live.ru>
2018-07-06 06:10:02 -0700

    Fix

--- functions.php
+++ functions.php
@@ -36,7 +36,7 @@ add_action( 'init', 'register_menus' );
 function register_menus() {
   register_nav_menus(
     array(
-      'header-menu' => __( 'Header Menu' , 'simplenevertheme')
+      'header-menu' => __( 'Header Menu' , 'simple-never-theme')
       //,'extra-menu' => __( 'Extra Menu' )
      )
    );
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php get_header() ?>
+<?php get_header(); ?>
 
 <!-- container start -->
 <div class="container">
